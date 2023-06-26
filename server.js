30d5595d355ee5113e6b6fe53eba90c2a90bcb2b
Elad Cohen <elad@q-biz.co.il>
2023-06-26 17:50:41 +0300

    node-fetch

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
-const PORT = 8000;
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const app = express()
+const PORT = process.env.PORT || 8000; // Use the PORT value from the environment or default to 8000
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const fetch = require('node-fetch'); // Add the 'node-fetch' package to make HTTP requests in Node.js
+const app = express();
 
-dotenv.config()
+dotenv.config();
 
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
+const OPEN_AI_SECRET = process.env.OPEN_AI_SECRET;
 
+app.post('/completions', async (req, res) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${OPEN_AI_SECRET}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: req.body.message }],
+      max_tokens: 100,
+    }),
+  };
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+    const data = await response.json();
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
-
-const OPEN_AI_SECRET=process.env.OPEN_AI_SECRET;
-app.post('/completions' , async (req,res) => {
-    const options = {
-        method: 'POST',
-        headers: {
-            "Authorization": `Bearer ${OPEN_AI_SECRET}`,
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*"
-        },
-        body: JSON.stringify({
-            model: 'gpt-3.5-turbo',
-            messages: [{role: "user" , content: req.body.message}],
-            max_tokens: 100
-        })
-    }
-    try{
-       const response = await fetch('https://api.openai.com/v1/chat/completions' , options)
-       const data = await response.json()
-       res.send(data)
-    } catch(err){
-        console.error(err)
-    }
-})
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`) )
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
