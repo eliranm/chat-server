4ee6b9572b69afe93049826e13947d1355108bad
Elad Cohen <elad@q-biz.co.il>
2023-06-26 17:33:31 +0300

    dotenv

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const PORT = process.env.PORT || 8000;
-const express = require('express')
-const cors = require('cors')
-
+const PORT = 8000;
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
 const app = express()
+
+dotenv.config()
 
 
 app.use(express.json())
 app.use(cors())
 
-const OPEN_AI_SECRET= process.env.OPEN_AI_SECRET
-
+const OPEN_AI_SECRET=process.env.OPEN_AI_SECRET;
 app.post('/completions' , async (req,res) => {
     const options = {
         method: 'POST',
