6c16858bb46331e3333df751e06cba7fb3b9651e
Elad Cohen <elad@q-biz.co.il>
2023-07-02 11:11:11 +0300

    .adjust req.body

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.post('/completions', async (req, res) => {
     },
     body: JSON.stringify({
       model: 'gpt-3.5-turbo',
-      messages: [{ role: 'user', content: req.body.message }],
+      messages: req.body.messages,
       max_tokens: 2000
     }),
   };
