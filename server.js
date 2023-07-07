1951cc667e5e420ad47d25c7496e5c03f98fa9af
Eliran Milo <eliran@q-biz.co.il>
2023-07-07 16:10:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -20,10 +20,9 @@ app.post('/completions', async (req, res) => {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      model: 'gpt-3.5-turbo-16k',
+      model: 'gpt-3.5-turbo',
       messages: req.body.messages,
-      max_tokens: 2000,
-      stream: true
+      max_tokens: 2000
     }),
   };
   try {
